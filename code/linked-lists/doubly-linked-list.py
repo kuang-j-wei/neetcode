1c4354be8d06725873f23666f370fcc5917aa8fe
kuang-j-wei <kuangwei0824@gmail.com>
2025-01-20 22:13:52 -0500

    Add dummy node solution to doubly linked list

--- code/linked-lists/doubly-linked-list.py
+++ code/linked-lists/doubly-linked-list.py
@@ -110,3 +110,85 @@ class DoublyLinkedList:
             self.tail.prev = None
             self.tail = tmp_prev
         self.size -= 1
+
+
+class DoublyLinkedListDummy:
+    def __init__(self):
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+    
+    def get(self, index):
+        if index >  self.size - 1:
+            return -1
+
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        
+        return curr.next.val
+    
+    def addAtHead(self, val):
+        new_node = Node(val)
+
+        curr_head = self.head.next
+        new_node.next = curr_head
+        curr_head.prev = new_node
+        
+        new_node.prev = self.head
+        self.head.next = new_node
+
+        self.size += 1
+    
+    def addAtTail(self, val):
+        new_node = Node(val)
+
+        curr_tail = self.tail.prev
+        new_node.prev = curr_tail
+        curr_tail.next = new_node
+
+        new_node.next = self.tail
+        self.tail.prev = new_node
+
+        self.size += 1
+    
+    def addAtIndex(self, index, val):
+        if index > self.size:
+            return None
+        
+        new_node = Node(val)
+
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        
+        new_node.next = curr.next
+        curr.next.prev = new_node
+        new_node.prev = curr
+        curr.next = new_node
+
+        self.size += 1
+    
+    def deleteAtIndex(self, index):
+        if index > self.size - 1:
+            return None
+    
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+        
+        
+        curr.next.next.prev = curr
+        curr.next = curr.next.next
+
+        self.size -= 1
+
+    def deleteAtTail(self, index):
+        if self.size == 0:
+            return None
+        
+        true_tail = self.tail.prev
+        true_tail.prev.next = true_tail.next
+        self.tail.prev = true_tail.prev
