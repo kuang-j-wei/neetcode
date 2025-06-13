78fab23be5574f21be8987b9762053a45432f319
kuang-j-wei <kuangwei0824@gmail.com>
2025-06-12 23:47:59 -0400

    Add O(n) bucket sort and min heap solution to top k frequent elements

--- code/roadmap-based/arrays-and-hashing/lc-347_top-k-frequent-elements.py
+++ code/roadmap-based/arrays-and-hashing/lc-347_top-k-frequent-elements.py
@@ -1,5 +1,106 @@
 from typing import List
 from collections import defaultdict
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        Solution:
+            We first count the number of occurrence of each unique
+            number
+
+            Then we construct another array where the index represents
+            how many times a number appears
+
+            This array is guaranteed to be at most of size n because
+            that would only happen if each element in nums is unique
+            
+            Since we already used a hashmap to count the occurrence, we
+            can just loop through the keys and directly index the count
+            array
+
+            Then in the count array, we can make each element a list,
+            and we can then just append the list with the unique number
+
+            Finally, to get k top frequent elements, we can iterate from
+            the back of that count array, until we've accumulated enough
+            unique numbers (the question states we are guaranteed to
+            have an unique solution so we don't have to worry about
+            the output array actually exceeding size k)
+
+        Time Complexity: O(n)
+            We do an O(n) pass for 3 times
+
+        Space Complexity: O(n)
+            We need count_dict and frequency each can take up to O(n)
+        """
+        length = len(nums)
+        count_dict = {}
+        frequency = [[] for i in range(length + 1)]
+        ans = []
+
+        for n in nums:
+            count_dict[n] = 1 + count_dict.get(n, 0)
+        
+        for n, count in count_dict.items():
+            frequency[count].append(n)
+
+        idx = length
+        while len(ans) < k:
+            ans.extend(frequency[idx])
+            idx -= 1
+
+        return ans
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        Solution:
+            We first count the occurrence of numbers using a hashmap.
+            
+            Then we construct a min heap, using the count as the sorting
+            key. When the heap is bigger than k, we know we need to
+            start kicking elements out. And since this is a min heap, if
+            we always kick out the smallest count number, then we know
+            at the end we will have the k biggest numbers left in the
+            heap
+
+            We can then just loop through the heap and collect the
+            unique numbers as our solution
+
+
+
+        Time Complexity: O(n log(k))
+            Because heappush can take log(k), and we have to do this n
+            times when we are constructing the min_heap
+        
+        Space Complexity: O(n + k) total space
+            O(n) because we still need to construct the count dict which
+            could at most be O(n)
+
+            Min heap will be of size O(k)
+        """
+        length = len(nums)
+        count_dict = {}
+        min_heap = []
+        ans = []
+
+        for n in nums:
+            count_dict[n] = 1 + count_dict.get(n, 0)
+        
+        for n, count in count_dict.items():
+            heapq.heappush(min_heap, (count, n))
+
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+
+        for _ in range(k):
+            count, n = heapq.heappop(min_heap)
+            ans.append(n)
+        return ans
+
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
