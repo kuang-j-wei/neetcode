239a0564b5f02311486c8cda9cd3439e9ad89437
Kuang Wei <kuangwei0824@gmail.com>
2025-06-20 18:04:31 -0700

    Implement O(n) solution for LC 128 longest consecutive sequence

--- code/roadmap-based/arrays-and-hashing/lc-128_longest-consecutive-sequence.py
+++ code/roadmap-based/arrays-and-hashing/lc-128_longest-consecutive-sequence.py
@@ -3,57 +3,85 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
+        """
+        Solution:
+            We do a three steps approach
 
+            On first scan, we collect all the unique numbers in the list
+            into a hash set
+
+            Then for each number in nums, we check if n - 1 exists. If
+            it doesn't exist, we know that it must be a sequence
+            beginner, and we save that sequence beginner into another
+            hash set
+
+            Then lastly, we just start counting up using each sequence
+            beginner. We count up as we check whether n+1 exists
+
+            ---
+
+            Alternatively, we could do the forward counting pass all
+            in one go instead of creating a sequence_beginner set.
+
+            We simply iterate over the nums list, and check if n - 1 is
+            in nums_hash. If not, we know a sequence beginner has been
+            encountered so we can count forward.
+
+            This looks like a O(n^2) nested loop, but that's not the
+            actual time cost. Because if a true sequence beginner is not
+            encountered, we would never enter the while loop
+
+            And for a non-sequence starter, we would only count it once
+            in the while loop
+
+            So at the end each num is only encountered once
+
+        Time Complexity: O(n)
+            Constructing the first unique num hash will take O(n)
+            as we need to iterate over nums
+
+            Then to create a set of sequence beginners, we again iterate
+            over nums and for each num, we check whether there is a
+            number smaller than it available via the nums_hash, each
+            check being an O(1) operation
+
+            Then lastly, we use the set beginner to count forward. Again
+            this would at most make us iterate through all numbers in
+            the list nums again so at most can take O(n)
+
+        Space Complexity: O(n)
+            We have to save two additional hash sets, which could each
+            be at most O(n)
+
+        """
         nums_hash = set(nums)
         max_length = len(nums_hash)
 
         if max_length == 1:
             return 1
-        
+        elif max_length == 0:
+            return 0
 
-        ans = 1
-        max_ans = 1
+        # sequence_beginner = set()
 
-        # beginning = min(nums_hash)
-        # nums_hash.remove(beginning)
+        # for n in nums_hash:
+        #     if n - 1 not in nums_hash:
+        #         sequence_beginner.add(n)
 
-        # while len(nums_hash) > 0:
-        #     if beginning + 1 in nums_hash:
-        #         # print(f"{beginning} + 1 found in {nums_hash}, incrementing {ans} by 1")
+        # max_ans = 1
+        # for n in sequence_beginner:
+        #     ans = 1
+        #     while n + 1 in nums_hash:
         #         ans += 1
-        #         max_ans = max(max_ans, ans)
-        #         nums_hash.remove(beginning + 1)
-        #         beginning += 1
-        #         # print(f"beginning set to {beginning}, nums_hash = {nums_hash}, with ans = {ans}\n")
-        #     else:
-        #         # print(f"{beginning} + 1 not found in {nums_hash}, setting beginning to {min(nums_hash)} and ans = 1")
-        #         beginning = min(nums_hash)
-        #         nums_hash.remove(beginning)
-        #         max_ans = max(max_ans, ans)
-        #         ans = 1
-        # return max_ans
-            
+        #         n += 1
+        #     max_ans = max(max_ans, ans)
 
+        max_ans = 1
         for n in nums:
-            # print(f"Starting a new search for {n}, current ans = {ans}")
-            ans = 1
-            head = n
-            tail = n
-            while ans < max_length:
-                if head + 1 in nums_hash:
-                    # print(f'Can extend {head} to {head + 1}, ans = {ans + 1}')
+            if n - 1 not in nums_hash:
+                ans = 1
+                while n + 1 in nums_hash:
                     ans += 1
-                    head += 1
-                    continue
-                if tail - 1 in nums_hash:
-                    # print(f'Can extend {tail} to {tail - 1}, ans = {ans + 1}')
-                    ans += 1
-                    tail -= 1
-                    continue
-                max_ans = max(ans, max_ans)
-                # print(f"No head or tail found, breaking. Current ans = {ans}, max_ans = {max_ans}")
-                break
-            continue
-        return max(ans, max_ans)
+                    n += 1
+                max_ans = max(max_ans, ans)
+        return max_ans
