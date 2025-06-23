0e4681349eca7b71e7e475652dea1b01456a2181
Kuang Wei <kuangwei0824@gmail.com>
2025-06-22 18:37:58 -0700

    Add backtracking solution for LC 22 generate parentheses

--- code/roadmap-based/backtracking/lc-22_generate-parentheses.py
+++ code/roadmap-based/backtracking/lc-22_generate-parentheses.py
@@ -4,63 +4,25 @@ from typing import List
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         """
-        The character length will always be n * 2
-
-        And at each character, we have a choice of left
-        or right bracket
-
-        We can generate this list of all possible string
-        combinations, and then validate whether the
-        parentheses are valid
-
-        Time Complexity: O(2 ^ (2n))
-
-        Space Complexity: O(2 ^ (2n))
+        Space Complexity: O(n)
+            O(n) possible recursive stacks for the depth of the tree
+            And also O(n) for the `curr_str` list
         """
-        results = self.generate_all_parentheses_recursive(n)
+        result = []
+        curr_str = []
 
-        final_answer = []
-        for result in results:
-            if self.parenthese_checker(result):
-                final_answer.append(result)
+        def backtrack(right_count, left_count):
+            if right_count == left_count == n:
+                result.append("".join(curr_str))  # this join will be O(2n)
+                return None
+            if right_count > left_count:
+                curr_str.append(')')
+                backtrack(right_count, left_count + 1)
+                curr_str.pop()
+            if right_count < n:
+                curr_str.append('(')
+                backtrack(right_count + 1, left_count)
+                curr_str.pop()
 
-        return final_answer
-
-    def generate_all_parentheses_recursive(self, n, current="", all_combinations=None):
-        """
-        Generate all possible combinations of parentheses using recursion
-
-        Args:
-            n: Half the length of the string (so total length is 2n)
-            current: Current string being built (used in recursion)
-            all_combinations: List to store all combinations (used in recursion)
-
-        Returns:
-            List of all possible parentheses combinations
-        """
-        if all_combinations is None:
-            all_combinations = []
-
-        # Base case: if we've reached the target length
-        if len(current) == 2 * n:
-            all_combinations.append(current)
-            return all_combinations
-
-        # Recursive case: try adding '(' and ')'
-        self.generate_all_parentheses_recursive(n, current + '(', all_combinations)
-        self.generate_all_parentheses_recursive(n, current + ')', all_combinations)
-
-        return all_combinations
-
-    def parenthese_checker(self, input_string):
-        stack = []
-
-        for char in input_string:
-            if len(stack) > 0 and char == ')' and stack[-1] == '(':
-                _ = stack.pop()
-            else:
-                stack.append(char)
-        if stack:
-            return None
-        else:
-            return input_string
+        backtrack(0, 0)
+        return result
