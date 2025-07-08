132100ed3cb61de9249d6f28c6fe25259ac8f6de
kuang-j-wei <kuangwei0824@gmail.com>
2025-07-08 00:40:58 -0400

    Add efficient O(n) solution to LC 556 next greater element III

--- code/roadmap-based/stacks/lc-556_next-greater-element-iii.py
+++ code/roadmap-based/stacks/lc-556_next-greater-element-iii.py
@@ -1,23 +1,48 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
-        # if you want to be  greeter, then the leading
-        # digit should be larger
-        # since we have the same amount of digits, we
-        # will have the same order of magnitude
-        # the largest integer with the same digits
-        # would just be a sorted list of these digits
-        # ordered in a descending order
-        # but we want the smallest
-        # 1197 -> 7119
-        # 1234 -> 1243
-        # 389274
-        # 389724
-        # i'm searching near the end of the digits, in order to find a
-        # digit that is larger
-        # so I'm starting a window from the end of size two, if a digit
-        # to the right is bigger, then I will swap it forward, and that
-        # is the answer
+        """
+        Solution:
+            We want to start from the back and start scanning. If we
+            find a new digit in the front that's smaller than any of the
+            previously seen digit, then we know that we need to swap
+            a digit in the back to the front
 
+            We do so by utilizing a stack. If the last position of the
+            stack is smaller than this newly scanned digit, then we
+            append it this new digit. This way the stack is ensured to
+            be in ann increasing order
+
+            Then the moment we see that a new digit is smaller than the
+            last position, we know that we now have a old digit that's
+            larger than this new digit
+
+            But we don't always want to move this old largest digit to
+            the front. We just want a old digit that's larger than this
+            current digit to move to the front
+
+            So we again start iterating through the stack but from the
+            front. If the new stack number is smaller than the current
+            digit, skip. If it's larger, then we have found the
+            candidate to swap with digit. So we append this candidate.
+
+            Then the rest of the digits should be what we have
+            previously scanned from the back, but we want it to be in
+            increasing order, and we have to add in this digit acquired
+            from digits. But since the stack is already guaranteed to be
+            in increasing order, and we already one by one checked from
+            the front of the stack that each number is smaller than
+            digit. Then we know we can just "insert" digit at the middle
+            and the stack will just be in increasing order
+
+        Time Complexity: O(n)
+            Because we are only scanning through digits. And even in
+            that inner loop, we won't iterate anything that's over the
+            length of n and the function will terminate there
+        
+        Space Complexity: O(n)
+            At worst we would keep a stack that's the same length as the
+            input array
+        """
         digits = [c for c in str(n)]
         length = len(digits)
         stack = [digits[-1]]
@@ -26,25 +51,28 @@ class Solution:
             digit = digits[i]
 
             if digit < stack[-1]:
-                # a previously seen digit is bigger, we can swap now,
-                # and leave the rest of the digits ordering before
-                # this index the same. But we now also have to make sure
-                # the rest of the backward counted digits are ordered
-                # from the smallest digit to the largest digit
-                # so really we could just order
-                big_digit = stack.pop()
-                stack.append(digit)
-                stack.append(big_digit)
-                # stack is reverse ordering
-
+                # finally a smaller number is found
+                # first save the digits in front
+                # then we search through the stack and find the smallest 
+                # number in the stack that's larger than this current
+                # And since the stack is already ensured to be in an
+                # ascending order, we know that we can then just insert
+                # digit at this point and it's 
                 digits = digits[:i]
-                digits.extend(stack[::-1])
-                return int("".join(digits))
+                for j in range(len(stack)):
+                    if stack[j] > digit:
+                        digits.append(stack[j])
+                        stack = stack[:j] + [digit] + stack[j+1:]
+                        stack.sort()
+                        digits.extend(stack)
+                        ans = int("".join(digits))
+                        return ans if ans <= (2 ** 31 - 1) else -1
+                return ans 
             else:
-                stack.append(digit)
+                stack.append(digit)  # we will be in increasing order
         return -1
 
 if __name__ == '__main__':
     s = Solution()
-    n = 230241
-    s.nextGreaterElement()
+    n = 2147483476
+    print(s.nextGreaterElement(n))
