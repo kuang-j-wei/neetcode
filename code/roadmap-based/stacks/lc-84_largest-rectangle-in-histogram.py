b7ea919c247c6e58488c4730463c4ab95f2c1858
kuang-j-wei <kuangwei0824@gmail.com>
2025-07-17 00:04:21 -0400

    Optimize minimum height calculation for LC 84

--- code/roadmap-based/stacks/lc-84_largest-rectangle-in-histogram.py
+++ code/roadmap-based/stacks/lc-84_largest-rectangle-in-histogram.py
@@ -5,9 +5,13 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
         max_area = 0
+        minimums = [[]]
         for i in range(n):
             for j in range(i + 1, n + 1):
                 width = j - i
-                height = min(heights[i:j])
+                if width == 1:
+                    height = heights[i]
+                else:
+                    height = min(height, heights[j - 1])
                 max_area = max(max_area, width * height)
         return max_area
