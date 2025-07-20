f0b8bc63ea16c060ffaf28902b7f58bfd84a5ec9
Kuang Wei <kuangwei0824@gmail.com>
2025-07-20 17:15:02 -0400

    Add faster O(n / 2) solution for LC 125 Valid Palindrome

--- code/roadmap-based/two-pointers/lc-125_valid-palindrome.py
+++ code/roadmap-based/two-pointers/lc-125_valid-palindrome.py
@@ -38,6 +38,53 @@ class Solution:
         return True
 
 
+class SolutionFast:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        Solution:
+            (Add one observation that for a palindrome it's a reflection
+            in the middle; and we can just increment the pointer
+            whenever we encounter a non-alphanumeric character)
+
+            I will have one pointer to increment from the front and one
+            from the back
+
+            If the characters don't agree, then return False. Otherwise,
+            if we manage to iterate the entire string then return True
+
+        Time Complexity: O(n)
+            We pass the string three times. One forward to remove white
+            spaces and non alphanumeric characters
+
+        Space Complexity: O(1)
+            No additional memory is used
+        """
+        i, j = 0, len(s) - 1
+        n = len(s)
+        while i <= j:
+            # increment if it's not alpha numeric
+            # also make sure we don't cross i and j
+            while i < j and not s[i].isalnum():
+                i += 1
+            while j > i and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+            else:
+                i += 1
+                j -= 1
+        return True
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    input_string = "A man, a plan, a canal: Panama"
+
+    print(s.isPalindrome(input_string))
+
+
 if __name__ == '__main__':
     s = Solution()
 
