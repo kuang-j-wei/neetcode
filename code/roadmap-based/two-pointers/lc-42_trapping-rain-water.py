f30727e108e4a3b304fedf5da4601491cc9be759
kuang-j-wei <kuangwei0824@gmail.com>
2025-09-30 01:03:55 -0400

    Add description for LC 42 Trapping Rain Water

--- code/roadmap-based/two-pointers/lc-42_trapping-rain-water.py
+++ code/roadmap-based/two-pointers/lc-42_trapping-rain-water.py
@@ -4,6 +4,27 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
         """
+        We will perform the search in a "two pass" logic
+
+        We use a left and right pointer, and start from the left to the
+        end. We keep the left pointer at the "left wall", and at each
+        step calculate the potential maximum water we could capture at
+        this location, which would be the height of the left wall minus
+        the current height. We continue doing so, until we reach a right
+        wall that's the same height or taller than the left wall. We can
+        then collect the water and then move the left wall to this
+        point.
+
+        Now, when we reach the end, the last wall very likely will not
+        be the same height or taller than the left wall. But we know
+        the current highest wall, which would be the current left wall
+        pointer. So we can then perform the same process, but start from
+        the end of the array, and go backwards until we hit the highest
+        wall. Again we will use a right and left pointer approach, with
+        the right pointer starting from the end of the array, and the
+        left pointer one position to the left of it. 
+
+
         Time: O(n)
             We at most pass the entire array twice (once forward once
             backward)
