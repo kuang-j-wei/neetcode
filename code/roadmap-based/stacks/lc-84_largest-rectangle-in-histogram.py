2a9c036b6bb14f909c58a3415b963ffb702b5597
Kuang Wei <kuangwei0824@gmail.com>
2025-10-02 21:39:44 -0400

    Add O(n) solution to LC 84

--- code/roadmap-based/stacks/lc-84_largest-rectangle-in-histogram.py
+++ code/roadmap-based/stacks/lc-84_largest-rectangle-in-histogram.py
@@ -6,26 +6,52 @@ class Solution:
         """
         We iterate forward, and keep adding bars to a stack, until we
         see a bar that's shorter than the most recent bar, which would
-        be the top of the stack.
+        be the top of the stack. In this approach, the stack will always
+        be in an increasing order.
 
-        Then we know that we have to count backwards, 
-        We will keep a stack of indices, where each index essentially
-        stores 'the index of the left wall'
+        Once a shorter bar is encountered, we know that the current
+        tallest bar can't be extend to the right anymore, so we can
+        start counting the area that iit can occupy. And the area it can
+        occupy is its own height, to the current pointer right boundary.
+
+        As we are going backwards (popping the stack), once we find a
+        bar that's shorter or the same height as the limiting height,
+        then we know that we can stop, because this is now a bar that
+        can be extended to the right. We also note this current left
+        pointer as the "index" of this bar that's getting added to the
+        stack. Because this represent how far back this right wall can
+        be extended to.
+
+        Once we've reached the end of the input array, if we have
+        elements left in the stack, we just need to calculate their
+        area by limiting height to width, which is the recorded left
+        pointer to the full length of the array
+
+        Time Complexity: O(n)
+            Each element is pushed and popped once into and from the
+            stack only once
+
+        Space Complexity: O(n)
+            The stack can at most grow to the same size as the input
+            array
+
+
         """
         max_area = 0
         stack = []
 
-        for i, curr_height in enumerate(heights):
-            start = i
-            while stack and curr_height < heights[stack[-1]]:  # can't extend to the right anymore, look back to the left
-                lookback_index = stack.pop()
-                limiting_height = heights[lookback_index]
-                width = i - lookback_index
-                max_area = max(max_area, width * limiting_height)
-                start = i
-            stack.append(start)
+        for i, h in enumerate(heights):
+            limiting_index = i
+            while stack and h < stack[-1][1]:  # can't extend to the right anymore, look back to the left
+                limiting_index, limiting_height = stack.pop()
+                width = i - limiting_index
+                max_area = max(max_area, limiting_height * width)
+            stack.append((limiting_index, h))
 
-        for index in stack:
-            max_area = max(max_area, heights[index] * (len(heights) - index))
-            
+        # forward pass
+        n = len(heights)
+        while stack:
+            idx, h = stack.pop()
+            width = n - idx
+            max_area = max(max_area, h * width)
         return max_area
