5b30a7ee542c4a00729451a1821778f4c0c43206
Kuang Wei <kuangwei0824@gmail.com>
2025-10-10 00:09:56 -0400

    Add tuple sorting attempt to LC 981

--- code/roadmap-based/binary-search/lc-981_time-based-key-value-store.py
+++ code/roadmap-based/binary-search/lc-981_time-based-key-value-store.py
@@ -22,6 +22,24 @@ class TimeMap:
         if key not in self.dict:
             return ""
 
+        self.dict[key].sort(key=lambda x: x[0])
+        sorted_list = self.dict[key]
+
+        l, r = 0, len(sorted_list) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if sorted_list[mid][0] == timestamp:
+                return sorted_list[mid][1]
+
+            if timestamp < sorted_list[mid][0]:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        # no direct match, then either original l r overlap pointer is
+        # the closest to the timestamp, or one less
+        return sorted_list[mid][1] if sorted_list[mid][0] < timestamp else sorted_list[mid-1][1]
+
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
