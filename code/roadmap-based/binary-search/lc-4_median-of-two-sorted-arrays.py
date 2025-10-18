ec0f1c5022a7cdf67c9abc3ad0499dcd3a8137f2
Kuang Wei <kuangwei0824@gmail.com>
2025-10-17 22:01:08 -0400

    Add LC4 neet code explanation

--- code/roadmap-based/binary-search/lc-4_median-of-two-sorted-arrays.py
+++ code/roadmap-based/binary-search/lc-4_median-of-two-sorted-arrays.py
@@ -11,18 +11,50 @@ class Solution:
         in the left partitions are smaller than the all elements in the
         right partition.
 
-        To do this, we will take the smaller array and do a middle
-        partition. Then in the other array, we just take the first x
-        elements, where x equals to the number of elements we need in
-        the left partition in order to divide the combined array into
-        half proportions.
+        Now call the smaller (or equal length) array A, the other B.
 
-        And when we find that the ordering condition is not met, we move
-        move the half partition point higher or lower, depending on the
-        breakage condition. If we know that the left partition numbers
-        are too big, we move to the left. Otherwise we move to the
-        right. We can even do this until we've covered the entire
-        smaller array, and we will then move to the next.
+        Now, regardless of whether the total length of the two arrays
+        adds up to even or odd, we are will need (total_length) // 2
+        elements in the left partition. So we will take the array that's
+        smaller and first find its mid point and take the start to this
+        midpoint (inclusive) as the left partition of A. We then just
+        need to take `half_length - (midpoint - 1)` amount of elements
+        from array B, which together would then form half_length.
+
+        We can then check the max and min value of these 4 sub
+        partitions to see if the median condition is met. If it is, then
+        for the even total length condition, the answer will be an
+        average of the last element of the left partition and the first
+        element of the right partition. The last element of the left
+        partition is simply max(leftA, leftB), and the first element of
+        the right partition is min(rightA, rightB). And if the total
+        length is odd, then we now that the way we've set up our
+        condition, the right partition will always be one element larger
+        than the left partition, because we forced the left partition to
+        be of length (total_length // 2), which rounds down. Thus we
+        just need to take the first element of the right partition,
+        which is min(rightA, rightB).
+
+        Note that as we are doing binary partition on the smaller array,
+        there is a chance that left and right pointers would at the edge
+        of the array. So then when we try to identify a left partition
+        (when mid point is already at -1) that doesn't exist or a right
+        partition that doesn't exist (mid point is at len(A)), we can
+        resolve this issue by assigning the min value as -inf and max
+        value as inf so that we will rely entirely on the other array
+        to check if the min max condition has been satisfied. Put
+        another way, in conditions like these we know the true median
+        lives purely only in the other array therefore the 4 partition
+        comparisons will have to be met, and by setting min to -inf and
+        max to inf we will make sure that the condition will always be
+        met.
+
+        Time Complexity: O(log(n)), n is the smaller length
+            We binary search on the smaller array, and once that binary
+            search is done the answer is also found, so no need to
+            binary search the other array
+        Space Complexity: O(1)
+            No additional space
         """
         m = len(nums1)
         n = len(nums2)
