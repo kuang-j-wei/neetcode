c1d4fbb44d558d130bf90d4bf3d0dba85fe587cf
kuang-j-wei <kuangwei0824@gmail.com>
2025-10-19 23:07:17 -0400

    Add O(n^2) solution to LC 3

--- code/roadmap-based/sliding-window/lc-3_longest-substring-without-repeating-characters.py
+++ code/roadmap-based/sliding-window/lc-3_longest-substring-without-repeating-characters.py
@@ -1,19 +1,8 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """
-        We will use a set to keep track of all the characters
-        that we've already encountered. Meanwhile we will track
-        the unique characters. And at each current pointer,
-        we will look to the next character. If that character
-        is already encountered, then we reset that set to the
-        current character, and rese the length counter to 1.
-
-        We will continue to do this until we've reached the
-        end of the array
-
-        At the current pointer, we look to the next character.
-        If that character is already previous encountered,
-        then we re
+        Time Complexity: O(n^2)
+        Space Complexity: O(n)
         """
         if len(s) <= 1:
             return len(s)
@@ -26,23 +15,13 @@ class Solution:
 
         while r < len(s):
             if s[r] in unique_counter:
-                print(f"{s[r]} already in {unique_counter}")
-                print(f"Setting l to {r - 1}")
-                if s[r - 1] != s[r]:
-                    l = r - 1
-                else:
-                    l = r
-                    r += 1
+                l += 1
+                r = l + 1
                 unique_counter = set(s[l])
-                continue
-                print(f"Setting unique_counter = {unique_counter}")
             else:
-                print(f"Found unique character {s[r]}")
                 unique_counter.add(s[r])
-                print(f"unique_counter is now {unique_counter}")
                 max_len = max(max_len, len(unique_counter))
                 r += 1
-                print(f"max_len = {max_len}")
 
         return max_len
 
