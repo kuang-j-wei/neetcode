2646fe0729c23925fbbd0509dc5fd790d53918c8
Kuang Wei <kuangwei0824@gmail.com>
2025-10-21 23:42:03 -0400

    Add explanation to LC 424

--- code/roadmap-based/sliding-window/lc-424_longest-repeating-character-replacement.py
+++ code/roadmap-based/sliding-window/lc-424_longest-repeating-character-replacement.py
@@ -1,6 +1,40 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         """
+        We gradually grow a window, and as we grow the window, we
+        constantly check a "validation condition". This condition is
+        met if window_size - most_frequent_count <= k because this
+        represents the minimum number of flips we would need to do
+        for all characters in this window to be the same character.
+
+        Once this validation condition is met, we can then evaluate the
+        current window size and check if this is the maximum we've seen.
+
+        If this is not met, we will shrink from the left until this
+        condition is met. Because currently the window is too big and
+        contains too many mismatched characters that we don't have
+        enough budget to flip. Since we have already checked all the
+        characters up to this point, we know that up to the right
+        boundary we've identified the possible longest substring. Since
+        we want to continue to explore to the right to see if there are
+        possible combination of the substring that can be longer, we
+        need to discard the left elements in order for us to grow to
+        the right.
+
+        Once the window has reached the right end of the string, we've
+        exhausted all options, and we can return the maximum length
+        of the window size that we've discovered in this process.
+
+        Time Complexity: O(n)
+            Every element is visited at most twice by the right and
+            left pointer of the sliding window. Each loop also has a
+            O(26) constant maximum value traversal, which doesn't scale
+            with input n
+
+        Space Complexity: O(1)
+            We are only using a fix length array of size 26 to keep
+            track of the frequency, so it doesn't grow with the input
+            string size
         """
         if len(s) == 1:
             return 1
@@ -13,24 +47,21 @@ class Solution:
         frequency_counter[ord(s[l]) - ord('A')] += 1
 
 
-        while r < len(s) and l < len(s):
+        while r + 1 < len(s):
             # expand
             r += 1
-            if r > len(s) - 1:
-                break
-            window_size = r - l + 1
             frequency_counter[ord(s[r]) - ord('A')] += 1
             most_frequent_count = max(frequency_counter)
 
-            # validate, and if not, shrink until valid
-            while window_size - most_frequent_count > k and l < len(s):
+            # validate, and if not, shrink until valid (widow_size = r - l + 1)
+            while r - l + 1 - most_frequent_count > k and l < len(s):
                     frequency_counter[ord(s[l]) - ord('A')] -= 1
                     l += 1
                     most_frequent_count = max(frequency_counter)
 
             # window is valid now, record max length
-            window_size = r - l + 1
-            max_len = max(max_len, window_size)
+            max_len = max(max_len, r - l + 1)
+
         return max_len
 
 
