650af2d8a0b5b541779d432333d004c6789f521c
kuang-j-wei <kuangwei0824@gmail.com>
2025-11-21 23:34:43 -0500

    Add recursive reverse linked list

--- code/roadmap-based/linked-list/lc-206_reverse-linked-list.py
+++ code/roadmap-based/linked-list/lc-206_reverse-linked-list.py
@@ -37,5 +37,10 @@ class Solution:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        return head
+        if head is None or head.next is None:
+            return head 
+        
+        prev = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return prev 
