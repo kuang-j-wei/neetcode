286912b73c34ef7426eed832efa8da7134aa4b67
kuang-j-wei <kuangwei0824@gmail.com>
2025-11-24 23:37:14 -0500

    Add iterative approach to inverting binary tree

--- code/roadmap-based/trees/lc-226_invert-binary-tree.py
+++ code/roadmap-based/trees/lc-226_invert-binary-tree.py
@@ -37,3 +37,55 @@ class Solution:
         root.right = inverted_left
         root.left = inverted_right
         return root
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        """
+        To invert the binary tree iteratively, we can do a breadth first
+        search approach. At every layer, we add its leaf nodes to the
+        queue so we can traverse through them in the next layer. We then
+        swap these two leaf nodes' positions, and we move onto the next
+        layer
+
+        Time Complexity: O(n)
+            Every node is visited once
+        
+        Space Complexity: O(n)
+            We store pointers to the nodes in a queue, so the additional
+            space needed would only depend on the size of the queue. If
+            the tree is highly unbalanced, then at each layer there
+            would only be two nodes. But for a balanced tree, the
+            deepest layer would have the most number of nodes at roughly
+            n/2 nodes. This is because at each layer we have 2^0, 2^1,
+            ..., 2^h nodes. Since the total number of nodes is 1 + 2 + 4
+            + ... + 2^h, n = 2^(h + 1) - 1
+
+            n = 2^(h + 1) - 1
+            n + 1 = 2^(h + 1)
+            log(n + 1) = h + 1
+            h = log(n+1) - 1
+
+            So the last layer is
+            2 ^ h = 2^(log(n+1) - 1) = 2^log(n+1) / 2 = (n + 1) / 2
+
+            Which simplifies to O(n / 2) = O(n)
+        """
+        from collections import deque
+        
+        queue = deque()
+        if root:
+            queue.append(root)
+
+        while queue: 
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+                
+                curr.left, curr.right = curr.right, curr.left
+
+        return root
