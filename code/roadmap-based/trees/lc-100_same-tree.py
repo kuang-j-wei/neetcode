aaaa5e7169f0d78f4bd471e1e85d24e9759582cd
kuang-j-wei <kuangwei0824@gmail.com>
2025-12-05 00:06:03 -0500

    Remove unnecessary try except block in LC 100 DFS

--- code/roadmap-based/trees/lc-100_same-tree.py
+++ code/roadmap-based/trees/lc-100_same-tree.py
@@ -41,14 +41,8 @@ class Solution:
             n1 = len(queue_1)
             n2 = len(queue_2)
             for _ in range(n1):
-                try:
-                    curr_1 = queue_1.popleft()
-                except:
-                    return False
-                try:
-                    curr_2 = queue_2.popleft()
-                except:
-                    return False
+                curr_1 = queue_1.popleft()
+                curr_2 = queue_2.popleft()
                 
                 if (curr_1 is None) ^ (curr_2 is None):
                     return False
@@ -112,3 +106,29 @@ class Solution:
             stack_2.append(curr_2.left)
             stack_2.append(curr_2.right)
         return True
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        """
+        We need to check if the two trees are exactly the same. We can
+        simply traverse the both trees, for example using depth first
+        search. Then as long as the two values don't match, only one
+        value is None, or we run out of nodes in one of the trees first,
+        it means the two are not the exact same trees. Otherwise, we
+        will successfully traverse the two trees fully, and we can then
+        return True
+
+        Time Complexity: O(n)
+            We traverse the trees each exactly once
+        
+        Space Complexity: O(h)
+            The DFS stack size will grow to the depth of the tree, which
+            in best case would be O(log(n)) for a balanced tree, and
+            worse case is O(n) for an extremely skewed tree
+        """
+        if p is None and q is None:
+            return True
+        if ((p is None) ^ (q is None)) or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
