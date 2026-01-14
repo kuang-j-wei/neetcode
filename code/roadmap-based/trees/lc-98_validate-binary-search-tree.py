4cb77d88493e261d196d202542f4cad6f4d3237d
kuang-j-wei <kuangwei0824@gmail.com>
2026-01-14 09:05:03 -0500

    Complete iterative solution to LC 98

--- code/roadmap-based/trees/lc-98_validate-binary-search-tree.py
+++ code/roadmap-based/trees/lc-98_validate-binary-search-tree.py
@@ -14,13 +14,35 @@ class Solution:
         """
         We can just recursively traverse through the entire tree, and
         at every node, check whether its left and right nodes satisfy
-        the BST condition. We can do this traversal with BFS, since
+        the BST condition. Note though, that we not only need to check
+        the immediate nodes' conditions, but we also need to make sure
+        that every node in the left and right subtree of a node is below
+        or greater than this node, respectively. So we have to memorize
+        a minimum and maximum range that a node is allowed to take on,
+        given its parent history. 
+
+        We will use a queue to perform BFS, and instead of only pushing
+        nodes themselves into the queue, we will also pass on the min
+        max range, and the min max range will get updated as we push
+        new nodes into the queue. For a left child, we know that the max
+        range is now limited by the current node's value. Conversely for
+        a right child, the min range is also limited by the current
+        node's value as well.
+        
+        
+        Time Complexity: O(n)
+            Every node is traversed once
+        
+        Space Complexity: O(w)
+            The queue can at most grow to the widest layer of the tree,
+            which could be best case O(1) in an extremely unbalanced
+            tree, or worst case O(n/2) for a balanced tree
         """
         queue = deque()
         queue.append(
             (
                 root,
-                (float(-inf), float(inf))
+                (float(-inf), float(inf))  # first node, don't need to have any limit to the range
             )
         )
 
@@ -28,28 +50,24 @@ class Solution:
             for _ in range(len(queue)):
                 curr, min_max_range = queue.popleft()
                 min_range, max_range = min_max_range
-                if curr.val < min_range or curr.val > max_range:
+                if curr.val <= min_range or curr.val >= max_range:
                     return False
 
                 if curr.left:
-                    if curr.val <= curr.left.val or (curr.left.val < min_range or curr.left.val > max_range):
-                        return False
                     queue.append(
                         (
                             curr.left,
                             (
-                                (min_range, min(curr.left.val, max_range))
+                                (min_range, min(curr.val, max_range))
                             )
                         )
                     )
                 if curr.right:
-                    if curr.val >= curr.right.val or (curr.right.val < min_range or curr.right.val > max_range):
-                        return False
                     queue.append(
                         (
                             curr.right,
                             (
-                                (max(curr.right.val, min_range), max_range)
+                                (max(curr.val, min_range), max_range)
                             )
                         )
                     )
