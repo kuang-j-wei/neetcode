f538d784bb5f0be95c7ba205a4b18e9bb31bfe33
Kuang Wei <kuangwei0824@gmail.com>
2026-01-21 18:16:35 -0500

    Add LC 124 solution

--- code/roadmap-based/trees/lc-124_binary-tree-maximum-path-sum.py
+++ code/roadmap-based/trees/lc-124_binary-tree-maximum-path-sum.py
@@ -17,11 +17,37 @@ class Solution:
         We most likely will need to start from a leaf node when counting
         the sum based off of that [1, 2, 3] example. Same with that
         root = [-10,9,20,null,null,15,7] example. We can start from node
-        7, then to 20, then it traces up to 20, then to 15. But if a
-        leaf node is negative, then we wouldn't want to include to
-        include it, unless it's the smallest negative in this entire
-        tree, which is possible too. If the addition of a node decreases
-        the current sum,
+        7, then to 20, then it traces up to 20, then to 15. A node will
+        either be a middle node in a path, meaning it connects the left
+        and right nodes' paths. Or it would be a middle node of a path.
         """
-        return None
+        max_path = float("-inf")
+
+        def dfs(curr):
+            nonlocal max_path
+            if not curr:
+                return 0
+
+            # if not curr.left and not curr.right:
+            #     return curr.val
+
+            left_path_sum = max(0, dfs(curr.left))
+            right_path_sum = max(0, dfs(curr.right))
+            # two cases, one is this is a middle node, so summing left
+            # and right and itself
+            middle_node_path = left_path_sum + right_path_sum + curr.val
+            # in this case, we've essentially terminated the path, so
+            # compare it to the global path max to see if
+            max_path = max(max_path, middle_node_path)
+
+
+            # second is this is just a passing node, which takes the max
+            # of either left path or right path
+            passing_node_path = curr.val + max(left_path_sum, right_path_sum)
+
+
+            return passing_node_path
+
+        passing_path = dfs(root)
+        return max(max_path, passing_path)
 
