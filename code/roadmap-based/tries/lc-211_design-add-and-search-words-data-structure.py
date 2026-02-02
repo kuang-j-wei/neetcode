20b6585c5391d02b018e111c46a9dbf19422444b
kuang-j-wei <kuangwei0824@gmail.com>
2026-02-01 21:02:46 -0500

    Add solution to LC 211 without fully comprehending logic

--- code/roadmap-based/tries/lc-211_design-add-and-search-words-data-structure.py
+++ code/roadmap-based/tries/lc-211_design-add-and-search-words-data-structure.py
@@ -16,61 +16,29 @@ class WordDictionary:
             if not curr.children[idx]:
                 curr.children[idx] = Node(c)
             curr = curr.children[idx]
+        curr.end_of_word = True
 
     def search(self, word: str) -> bool:
         """
-        For '.', it doesn't matter what the current node is, as long as
-        it's not empty. If it's empty, then we return False (so we might
-        need a "children empty" indicator). And we would need to move
-        onto the next character after '.'. So we would need to skip the
-        current layer of the Trie, and search the next. We could do this
-        by iterating through all the current node that was previous on
-        '.' and search through their children. If any of their child
-        contains this character, then we can continue to iterate
-        forward.
+        Explanation to be filled.
         """
-        curr = self.root
-        prev_is_dot = False
-        consecutive_dots = False
-        consecutive_dots_found = False
-        for c in word:
-            if consecutive_dots:
-                idx = ord(c) - ord('a')
-                
-                for n in curr.children:
-                    if n:
-                        for nc in n.children:
-                            if nc and nc.children[idx]:
-                                consecutive_dots_found = True
-                                consecutive_dots = False
-                                prev_is_dot = False
-                                curr = nc.children[idx]
-                                break
-                    if consecutive_dots_found:
-                        break    
-
-            elif prev_is_dot and c != '.':
-                idx = ord(c) - ord('a')
-                for n in curr.children:
-                    if n and n.children[idx]:
-                        curr = n.children[idx]
-                        prev_is_dot = False
-                        break
-            elif prev_is_dot and c == '.':
-                consecutive_dots = True
-                continue
-            else:                            
+        def dfs(curr, word):
+            for i, c in enumerate(word):
                 if c == '.':
-                    prev_is_dot = True
-                    continue                    
-                    
-                idx = ord(c) - ord('a')
-                if not curr.children[idx]:
+                    for n in curr.children:
+                        if n and dfs(n, word[i + 1:]):
+                            return True
                     return False
+                    
                 else:
-                    curr = curr.children[idx]
-        return True 
+                    idx = ord(c) - ord('a')
+                    if not curr.children[idx]:
+                        return False
+                    else:
+                        curr = curr.children[idx]
+            return curr.end_of_word
 
+        return dfs(self.root, word)
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
