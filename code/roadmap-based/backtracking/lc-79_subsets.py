014d9ab76569ccf74fc5daa4eb53344ddf60d9b1
kuang-j-wei <kuangwei0824@gmail.com>
2026-03-04 01:51:56 -0500

    Add full LC 79 solution with explanation

--- code/roadmap-based/backtracking/lc-79_subsets.py
+++ code/roadmap-based/backtracking/lc-79_subsets.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         """
-        We can iterate though each element of nums, and at each element
+        We can recurse though each element of nums, and at each element
         we make a decision on yes/no of whether we include this element
         in a subset. This forms a binary decision tree of depth n, and
         at each layer we double the number of nodes. Then a subset is
@@ -12,17 +12,25 @@ class Solution:
         that sublist to the power set.
         
         Time Complexity: O(n * 2^n)
-            For each digit we have a yes/no decision, which depends on
-            the length of a sublist, would then take 2^n combinations.
-            Then each sublist can contain up to n elements, 
+            For each digit we have a yes/no decision, so we branch off two
+            cursive calls, giving us 2^n leaf nodes in total. And
+            because we need to keep a record of the sublists through the
+            process of dfs traversal so that we can collect the sublist
+            when we hit a leaf node, in our implementation we are
+            sending a copy of the running sublist when we pass to a DFS
+            function. This means that by the time we hit the last layer,
+            we will have 2^n nodes to process, and each will at most
+            have to copy a sublist with length n. Thus the total time
+            complexity is O(n * 2^n)
         
         Space Complexity: O(n * 2^n)
             The recursive stack would build up to a depth of O(n). But
             we are also passing `sublist` to each of the recursive dfs
             call, and each of the sublist could at most build up to the
             length of O(n), and in the last layer, there are O(2^n)
-            nodes, each with a sublist that's of length n, so the total
-            space complexity is of O(n * 2^n)
+            nodes, each with a sublist that's at most of length n, with
+            an average length of 2/n. So the space complexity is
+            O(n/2 * 2^n), which simplifies to O(n * 2^n)
         """
         self.res = []
         self.nums = nums
